3d9a82537b8d60ea7d1418cae55b9cb19cfac9a6
sonal <sonalkarle01@gmail.com>
2021-03-17 23:36:41 +0530

    Ability to count of persom in list

--- NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -42,69 +42,8 @@ namespace NUnitTestProject1
 
         }
         
-        /// <summary>
-        /// Uc3: Add employee in to the list
-        /// </summary>
-        [Test]
-        public void GivenEmployee_onpost_shouldreturnAddEmployee()
-        {
-            RestRequest request = new RestRequest("/AddressBook", Method.POST);
-            JObject jObject = new JObject();
-            jObject.Add("firstname", "Sona");
-            jObject.Add("lastname", "karle");
-            jObject.Add("Address", "Kurla");
-            jObject.Add("City" , "Mumbai");
-            jObject.Add("Zip", "400 066");
-            jObject.Add("Phonenumber", "98061840");
-
-
-            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
-            Contact dataresponse = JsonConvert.DeserializeObject<Contact>(response.Content);
-            Assert.AreEqual("Sona", dataresponse.firstname);
-            Assert.AreEqual("87549612", dataresponse.Phonenumber);
-
-           
-
-        }
-        
-        /// <summary>
-        /// UC4:UC4: Edit the details of employee
-        /// </summary>
-        [Test]
-        public void GivenEmployee_OnUpdate_shouldreturn_Updateemployee()
-        {
-            RestRequest request = new RestRequest("/AddressBook/5", Method.PUT);
-            JObject jObject = new JObject();
-            jObject.Add("firstname", "Priya");
-            jObject.Add("lastname", "Chavan");
-
-            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            Contact dataresponse = JsonConvert.DeserializeObject<Contact>(response.Content);
-            Assert.AreEqual("Manisha", dataresponse.firstname);
-            Assert.AreEqual("Shinde", dataresponse.Phonenumber);
-           
-        }
-        /// <summary>
-        /// UC5:Delete the person details
-        /// </summary>
-        [Test]
-        public void GivenEmployeeID_OnDelete_shouldreturnsucessFulstatus()
-        {
-            RestRequest request = new RestRequest("/AddressBook/6", Method.DELETE);
-
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            
-
-        }
-        
+       
+               
 
     }
 }
